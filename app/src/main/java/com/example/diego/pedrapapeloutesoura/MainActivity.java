632f073898c0ac69784cc0c3af30cc0de58fa836
Diego Nunes Reis <diegonunesftw@gmail.com>
2018-10-06 01:44:23 -0300

    Some adjustments

--- app/src/main/java/com/example/diego/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/example/diego/pedrapapeloutesoura/MainActivity.java
@@ -17,62 +17,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void escolhaPedra(View view){
+    public void escolhaPedra(View view) {
         realizarJogada("pedra");
     }
 
-    public void escolhaPapel(View view){
+    public void escolhaPapel(View view) {
         realizarJogada("papel");
     }
 
-    public void escolhaTesoura(View view){
+    public void escolhaTesoura(View view) {
         realizarJogada("tesoura");
     }
 
-    public void realizarJogada(String jogada){
-        String jogadas[] = { "pedra", "papel", "tesoura"};
+    public void realizarJogada(String jogada) {
+        String jogadas[] = {"pedra", "papel", "tesoura"};
         ImageView imagemAdversario = findViewById(R.id.escolhaAdversario);
 
         String escolhaAdversaria = jogadas[new Random().nextInt(3)];
 
-        if(escolhaAdversaria == "pedra"){
-            imagemAdversario.setImageResource(R.drawable.pedra);
-        } else if(escolhaAdversaria == "papel"){
-            imagemAdversario.setImageResource(R.drawable.papel);
-        }
-        else {
-            imagemAdversario.setImageResource(R.drawable.tesoura);
+        switch (escolhaAdversaria) {
+            case "pedra":
+                imagemAdversario.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imagemAdversario.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imagemAdversario.setImageResource(R.drawable.tesoura);
+                break;
         }
 
-        if(escolhaAdversaria == jogada){
+        if ((escolhaAdversaria == "pedra" && jogada == "papel") ||
+                (escolhaAdversaria == "papel" && jogada == "tesoura") ||
+                (jogada == "pedra" && escolhaAdversaria == "tesoura")) {
+            exibeResultado("vitoria");
+        } else if ((escolhaAdversaria == "pedra" && jogada == "tesoura") ||
+                (jogada == "pedra" && escolhaAdversaria == "papel") ||
+                (jogada == "papel" && escolhaAdversaria == "tesoura")) {
+            exibeResultado("derrota");
+        } else {
             exibeResultado("empate");
-        } else if (escolhaAdversaria == "pedra" && jogada == "papel"){
-            exibeResultado("vitoria");
-        } else if (escolhaAdversaria == "papel" && jogada == "tesoura"){
-            exibeResultado("vitoria");
-        } else if (jogada == "pedra" && escolhaAdversaria == "tesoura"){
-            exibeResultado("vitoria");
-        } else if (escolhaAdversaria == "pedra" && jogada == "tesoura"){
-            exibeResultado("derrota");
-        } else if (jogada == "pedra" && escolhaAdversaria == "papel"){
-            exibeResultado("derrota");
-        } else if (jogada == "papel" && escolhaAdversaria == "tesoura"){
-            exibeResultado("derrota");
         }
     }
 
-    public void exibeResultado(String resultado){
+    public void exibeResultado(String resultado) {
         TextView textoResultado = findViewById(R.id.textViewResultado);
         String textosDerrota[] = {"Você perdeu :(", "Que pena! tente mais uma vez", "Não foi dessa vez :/"};
         String textosVitória[] = {"Yes! você mandou ver!", "Você venceu!", "Wow, você venceu! =O"};
 
         int numeroTexto = new Random().nextInt(3);
 
-        if(resultado == "empate"){
+        if (resultado == "empate") {
             textoResultado.setText("Empatou! =O");
-        } else if(resultado == "vitoria") {
+        } else if (resultado == "vitoria") {
             textoResultado.setText(textosVitória[numeroTexto]);
-        } else{
+        } else {
             textoResultado.setText(textosDerrota[numeroTexto]);
         }
 
